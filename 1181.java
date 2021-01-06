786f73bdd7e4d477a579665659b3c26d34a2f33b
haejun <dubu4050@naver.com>
2021-01-06 20:15:04 +0900

    2021.01.05
    1181 단어 정렬

--- 1181.java
+++ 1181.java
@@ -11,9 +11,8 @@ class Main{
         TreeSet<String> list = new TreeSet<>(new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if(o1.length()==o2.length()) {
+                if(o1.length()==o2.length()) 
                     return o1.compareTo(o2);
-                }
                 return o1.length()<o2.length()?-1:1;
             }
         });
