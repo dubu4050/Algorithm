b6ad33d9896594377c77faf2fdfb6afea9e08daf
haejun <dubu4050@naver.com>
2021-01-06 20:21:58 +0900

    2021.01.03
    2751 수 정렬하기2

--- 2751.java
+++ 2751.java
@@ -9,12 +9,14 @@ class Main {
         int N = Integer.parseInt(br.readLine());
         ArrayList<Integer> arr = new ArrayList<>();
         StringBuilder stringBuilder = new StringBuilder();
-        for(int i=0;i<N;i++)
+        for(int i=0;i<N;i++){
             arr.add(Integer.parseInt(br.readLine()));
+        }
         Collections.sort(arr);
 
-        for(int i:arr)
+        for(int i:arr){
             stringBuilder.append(i).append('\n');
+        }
         System.out.println(stringBuilder.toString());
     }
 }
