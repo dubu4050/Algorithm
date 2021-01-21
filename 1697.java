ab7373f11fee9cc2e7a11c99789b8164f0140831
haejun <dubu4050@naver.com>
2021-01-21 13:17:07 +0900

    2021.01.21
    1697 숨바꼭질

---
+++ 1697.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, K;
+    static ArrayList<Integer>[] arrayList;
+    static Queue<Integer> queue = new LinkedList<>();
+    static int[] visited;
+    static int result = Integer.MAX_VALUE;
+    static int MAX_LEN = 100001*2;
+
+    static void bfs(){
+        queue.add(N);
+        visited[N]=1;
+        while(!queue.isEmpty()){
+            int tmp = queue.poll();
+            if(tmp == K){
+                result = Math.min(result, visited[K]-1);
+            }
+            int tmpCnt = visited[tmp];
+            Iterator<Integer> iter = arrayList[tmp].listIterator();
+            while(iter.hasNext()) {
+                int w = iter.next();
+                if(w < MAX_LEN) {
+                    if (visited[w] != 0 && visited[w] <= tmpCnt + 1) continue;
+                    queue.add(w);
+                    visited[w] = tmpCnt + 1;
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arrayList = new ArrayList[MAX_LEN];
+        visited = new int[MAX_LEN];
+
+        for(int i=0;i<MAX_LEN;i++)
+            arrayList[i] = new ArrayList<>();
+
+        arrayList[0].add(1);
+
+        for(int i=1;i<MAX_LEN;i++) {
+            arrayList[i].add(i*2);
+            arrayList[i].add(i+1);
+            arrayList[i].add(i-1);
+        }
+        bfs();
+        System.out.println(result);
+    }
+}
